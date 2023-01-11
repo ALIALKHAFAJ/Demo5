80532c9352eba298aa10c635db53592faff11130
Owner <Owner@DESKTOP-23C0TJ9.fios-router.home>
2023-01-10 22:53:02 -0500

    I did

---
+++ src/com/speed/practice/GitHubwork.java
@@ -0,0 +1,10 @@
+package com.speed.practice;
+
+public class GitHubwork {
+	
+	public static void main(String[] args) {
+		String x="h";
+		System.out.println(x);
+	}
+
+}
---
+++ src/com/speed/practice/Sport.java
@@ -0,0 +1,13 @@
+package com.speed.practice;
+
+public class Sport {
+	
+	public static void main(String[] args) {
+		int age = 45;
+		
+		int tax= 52;
+		
+		System.out.println("age,"+"tax");
+	}
+
+}
